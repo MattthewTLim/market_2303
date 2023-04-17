bcfd4a9a29745e9e346a65622a20c5415337d323
Matthew Lim <matlim200@gmail.com>
2023-04-17 09:09:44 -0600

    initial commit

---
+++ spec/vendor_spec.rb
@@ -0,0 +1,17 @@
+require './lib/item'
+require './lib/vendor'
+
+describe "#exists" do
+  it "exists" do
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+
+    expect(item1).to be_an_instance_of(Item)
+  end
+
+  it "has readable attributes" do
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    expect(item2.name).to eq("Tomato")
+    expect(item2.price).to eq(0.5)
+  end
+end
